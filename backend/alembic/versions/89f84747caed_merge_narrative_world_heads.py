30012cc5ccd410578be4320382949b6bfd557430
gqy20 <qingyu_ge@foxmail.com>
2026-03-20 19:32:12 +0800

    fix: merge alembic heads for narrative world

---
+++ backend/alembic/versions/89f84747caed_merge_narrative_world_heads.py
@@ -0,0 +1,20 @@
+"""merge narrative world heads
+
+Revision ID: 89f84747caed
+Revises: 20260312_000003, 20260319_000002
+Create Date: 2026-03-20 19:31:00.035883
+"""
+
+# revision identifiers, used by Alembic.
+revision = "89f84747caed"
+down_revision = ("20260312_000003", "20260319_000002")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
